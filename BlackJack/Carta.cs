f553605c67d187efbf685c9d29d098019455e320
Agustín Clemente <cagustinclemente@gmail.com>
2018-11-12 23:02:58 -0300

    Archivos iniciales
    Clase Carta funcional

---
+++ BlackJack/Carta.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Carta
+    {
+        int tipo;
+        int numero;
+        string nombre;
+        public Carta(int t, int n)
+        {
+            tipo = t;
+            numero = n;
+            nombre = SetNombre();
+        }
+
+        public string Nombre { get => nombre;}
+
+        private string SetNombre()
+        {
+            string nom = "";
+            switch (numero)
+            {
+                case 1:
+                    nom = "As";
+                    break;
+                case 2:
+                    nom = "Dos";
+                    break;
+                case 3:
+                    nom = "Tres";
+                    break;
+                case 4:
+                    nom = "Cuatro";
+                    break;
+                case 5:
+                    nom = "Cinco";
+                    break;
+                case 6:
+                    nom = "Seis";
+                    break;
+                case 7:
+                    nom = "Siete";
+                    break;
+                case 8:
+                    nom = "Ocho";
+                    break;
+                case 9:
+                    nom = "Nueve";
+                    break;
+                case 10:
+                    nom = "Diez";
+                    break;
+                case 11:
+                    nom = "Jack";
+                    break;
+                case 12:
+                    nom = "Reina";
+                    break;
+                case 13:
+                    nom = "Rey";
+                    break;
+            }
+            switch (tipo)
+            {
+                case 1:
+                    nom += " de picas";
+                    break;
+                case 2:
+                    nom += " de corazones";
+                    break;
+                case 3:
+                    nom += " de diamantes";
+                    break;
+                case 4:
+                    nom += " de tréboles";
+                    break;
+            }
+            return nom;
+        }
+    }
+}
---
+++ BlackJack/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            Carta carta = new Carta(rnd.Next(1, 5), rnd.Next(1, 14));
+            textBox1.Text = carta.Nombre;
+        }
+    }
+}
